d565e382233e0b19a8edab1b14e39c1aca6b2e35
Salman Sayeed <salmansayeed5345@gmail.com>
2021-12-17 22:46:45 +0600

    Code formatting

--- Assignment4/BSTInterface.java
+++ Assignment4/BSTInterface.java
@@ -1,15 +1,9 @@
 package Assignment4;
 
 public interface BSTInterface<E> {
-
     public void Insert(E value);
-
     public E Delete(E value);
-
     public boolean Find(E value);
-
     public void Traversal(String type);
-
     public void printBST();
-
 }
--- Assignment4/BSTMain.java
+++ Assignment4/BSTMain.java
@@ -5,105 +5,58 @@ public class BSTMain <E> implements BSTInterface{
     private Node root;
     private int nodecount;
 
-    BSTMain()
-    {
+    BSTMain(){
         root=null;
         nodecount=0;
     }
-
-
     @Override
     public void Insert(Object value) {
         root=inserthelp(root, (E) value);
         nodecount++;
     }
 
-    private Node inserthelp(Node<E> rt,E value)
-    {
-        if(rt==null)
-        {
-            return new Node(value);
-        }
-        if(rt.compareTo(value)>0)
-        {
-           rt.setLeft(inserthelp(rt.getLeft(),value));
-        }
-        else
-        {
-            rt.setRight(inserthelp(rt.getRight(),value));
-        }
+    private Node inserthelp(Node<E> rt,E value){
+        if(rt==null) return new Node(value);
+        if(rt.compareTo(value)>0) rt.setLeft(inserthelp(rt.getLeft(),value));
+        else rt.setRight(inserthelp(rt.getRight(),value));
         return rt;
     }
-
-
     @Override
     public Object Delete(Object value) {
         E temp= (E) findhelp(root, (E) value);
 
-        if(temp!=null)
-        {
+        if(temp!=null){
             root=removehelp(root, (E) value);
             nodecount--;
         }
         return temp;
     }
-
-    private Node<E> getMax(Node<E> rt)
-    {
-        if(rt.getRight()==null)
-        {
-            return rt;
-        }
-        else
-        {
-            return getMax(rt.getRight());
-        }
+    private Node<E> getMax(Node<E> rt){
+        if(rt.getRight()==null) return rt;
+        return getMax(rt.getRight());
     }
 
-    private Node<E> deleteMax(Node<E>rt)
-    {
-        if(rt.getRight()==null)
-        {
-            return rt.getLeft();
-        }
-
-            rt.setRight(deleteMax(rt.getRight()));
-            return rt;
+    private Node<E> deleteMax(Node<E>rt){
+        if(rt.getRight()==null) return rt.getLeft();
+        rt.setRight(deleteMax(rt.getRight()));
+        return rt;
 
     }
 
-    private Node<E> removehelp(Node<E> rt,E value)
-    {
-        if(rt==null)
-        {
-            return null;
-        }
-        if(rt.compareTo(value)>0)
-        {
-           rt.setLeft(removehelp(rt.getLeft(),value));
-        }
-        else if(rt.compareTo(value)<0)
-        {
-            rt.setRight(removehelp(rt.getRight(),value));
-        }
-        else
-        {
-            if(rt.getLeft()==null)
-            {
-                return rt.getRight();
-            }
-            else if(rt.getRight()==null)
-            {
-                return rt.getLeft();
-            }
-            else
-            {
+    private Node<E> removehelp(Node<E> rt,E value){
+        if(rt==null) return null;
+        
+        if(rt.compareTo(value)>0) rt.setLeft(removehelp(rt.getLeft(),value));
+
+        else if(rt.compareTo(value)<0) rt.setRight(removehelp(rt.getRight(),value));
+        
+        else{
+            if(rt.getLeft()==null) return rt.getRight();
+            else if(rt.getRight()==null) return rt.getLeft();
+            else{
                 Node temp=getMax(rt.getLeft());
-
                 rt.setValue((E) temp.getValue());
-
                 rt.setLeft(deleteMax(rt.getLeft()));
-
             }
         }
         return rt;
@@ -112,30 +65,23 @@ public class BSTMain <E> implements BSTInterface{
 
     @Override
     public boolean Find(Object value) {
-        if(findhelp(root, (E) value)==null)
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
+        if(findhelp(root, (E) value)==null) return false;
+        
+        else return true;
+    
     }
 
     @Override
     public void Traversal(String type) {
-        if(type.equalsIgnoreCase("in"))
-        {
+        if(type.equalsIgnoreCase("in")){
           inOrder(root);
           System.out.println();
         }
-        else if(type.equalsIgnoreCase("pre"))
-        {
+        else if(type.equalsIgnoreCase("pre")){
           preOrder(root);
           System.out.println();
         }
-        else
-        {
+        else{
            postOrder(root);
            System.out.println();
         }
@@ -147,86 +93,42 @@ public class BSTMain <E> implements BSTInterface{
         System.out.println();
     }
 
-    public static<E> void preOrder(Node node)
-    {
-        if (node == null)
-        {
-            return;
-        }
-
+    private static<E> void preOrder(Node node){
+        if (node == null) return;
         System.out.print(node.getValue() + " ");
-
         preOrder(node.getLeft());
-
         preOrder(node.getRight());
     }
-    public static<E> void postOrder(Node node)
-    {
-        if (node == null)
-        {
-            return;
-        }
-
+    private static<E> void postOrder(Node node){
+        if (node == null) return;
         postOrder(node.getLeft());
-
         postOrder(node.getRight());
-
         System.out.print(node.getValue() + " ");
     }
-    public static<E> void inOrder(Node node)
-    {
-        if (node == null)
-        {
-            return;
-        }
+    private static<E> void inOrder(Node node){
+        if (node == null) return;
         inOrder(node.getLeft());
-
         System.out.print(node.getValue() + " ");
-
         inOrder(node.getRight());
     }
 
 
 
-    private E findhelp(Node<E>rt,E value)
-    {
-        if(rt==null)
-        {
-            return null;
-        }
-        if(rt.compareTo(value)>0)
-        {
-            return (E) findhelp(rt.getLeft(),value);
-        }
-        else if(rt.compareTo(value)==0)
-        {
-            return rt.getValue();
-        }
-        else
-        {
-            return (E) findhelp(rt.getRight(),value);
-        }
+    private E findhelp(Node<E>rt,E value){
+        if(rt==null) return null;
+        if(rt.compareTo(value)>0) return (E) findhelp(rt.getLeft(),value);
+        else if(rt.compareTo(value)==0) return rt.getValue();
+        else return (E) findhelp(rt.getRight(),value);
     }
 
-    public static<E> void Print(Node node)
-    {
-        if(node == null)
-        {
-            return;
-        }
-
+    public static<E> void Print(Node node){
+        if(node == null) return;
         System.out.print(node.getValue());
-
-        if ((node.getLeft() == null) && (node.getRight() == null))
-        {
-            return;
-        }
-
+        if ((node.getLeft() == null) && (node.getRight() == null)) return;
 
         System.out.print("(");
         Print(node.getLeft());
         System.out.print(")");
-
 
         System.out.print("(");
         Print(node.getRight());
